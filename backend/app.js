284e49514ee45673ce8cbc1c2a4ab153fdff5af5
Himanshu behera <himanshubehera08052003@gmil.com>
2024-03-19 20:19:58 +0530

    replace

--- backend/app.js
+++ backend/app.js
@@ -9,7 +9,7 @@ const app = express();
 dotenv.config({path: "./config/config.env"});
 
 app.use(cors({
-    origin:[process.env.FRONTEND_URI],
+    origin:[""],
     methods: ["POST"],
     credentials:true,
 }));
