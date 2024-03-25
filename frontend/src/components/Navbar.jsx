acc273b40d92898f11d595c4b29bd42de46d560f
Himanshu behera <himanshubehera08052003@gmil.com>
2024-03-25 21:39:48 +0530

    update

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -2,32 +2,37 @@ import React, { useState } from "react";
 import { Link } from "react-router-dom";
 import { GiHamburgerMenu } from "react-icons/gi";
 import { data } from "../restApi.json";
+import { Link as ScrollLink, animateScroll as scroll } from "react-scroll";
 
 function Navbar() {
   const [show, setShow] = useState(false);
+
+  const toggleMenu = () => {
+    setShow(!show);
+  };
+
   return (
     <nav>
       <div className="logo">Foodhub</div>
-      <div className={show ? "navLink showmenu" : "navLinks"}>
+      <div className={show ? "navLinks showmenu" : "navLinks"}>
         <div className="links">
-          {data[0].navbarLinks.map((element) => {
-            return (
-              <Link
-                to={element.link}
-                key={element.id}
-                spy={true}
-                smooth={true}
-                duration={500}
-              >
-                {element.title}
-              </Link>
-            );
-          })}
+          {data[0].navbarLinks.map((element) => (
+            <ScrollLink
+              to={element.link}
+              key={element.id}
+              spy={true}
+              smooth={true}
+              duration={500}
+              onClick={toggleMenu}
+            >
+              {element.title}
+            </ScrollLink>
+          ))}
         </div>
         <button className="menuBtn">OUR MENU</button>
       </div>
-      <div className="hamburger" onClick={()=> setShow(!show)}>
-        <GiHamburgerMenu/>
+      <div className="hamburger" onClick={toggleMenu}>
+        <GiHamburgerMenu />
       </div>
     </nav>
   );
